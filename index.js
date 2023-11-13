633e0997031510cb69ab9d910094046571827acc
Sackas <valdir-ribeiro@outlook.com>
2023-11-13 18:45:05 -0300

    banners adicional

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var Samantha = window.document.getElementById("Samantha")
 var Bruna = window.document.getElementById("Bruna")
 var setaEsquerda = window.document.getElementById("seta-esquerda")
 
+var banner1 = window.document.getElementById("banner1")
+var banner2 = window.document.getElementById("banner2")
 
 function RolarParaDireita() {
     Leonardo.style = "display:none"
@@ -16,5 +18,22 @@ function RolarParaEsquerda() {
     Leonardo.style = "display: flex"
     Bruna.style = "display: none"
     setaDireita.style = "display:flex; margin-top:55px"
+    setaEsquerda.style = "display:none"
+}
+
+
+
+function RolarParaDireita2() {
+    banner1.style = "display:none"
+    banner2.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex;"
+}
+
+function RolarParaEsquerda2() {
+    
+    banner1.style = "display:flex"
+    banner2.style = "display:none"
+    setaDireita.style = "display:flex;"
     setaEsquerda.style = "display:none"
 }
